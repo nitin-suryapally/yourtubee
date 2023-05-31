e7f00a55a26c73d565ccc3295f2667bdb98878d5
Nitin Suryapally <nitin28suryapally@gmail.com>
2023-05-31 20:23:00 +0530

    youtube clone deployed

---
+++ src/components/Videos.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Stack } from '@mui/material'
+import { Box } from '@mui/material'
+import VideoCard from './VideoCard'
+import ChannelCard from './ChannelCard'
+import Loader from './Loader'
+
+const Videos = ({ videos , direction }) => {
+    // console.log(videos);
+    if(!videos?.length) return <Loader />
+    return (
+        <Stack
+            direction={ direction || "row"}
+            flexWrap="wrap"
+            justifyContent="start"
+            alignItems="start"
+            gap={2}
+        >
+
+            {videos.map((item, idx) => (
+                <Box
+                    key={idx}
+                >
+                    {item.id.videoId && <VideoCard video={item} />}
+                    {item.id.channelId && <ChannelCard channelDetail={item} />}
+                </Box>
+            ))}
+
+        </Stack>
+    )
+}
+
+export default Videos
